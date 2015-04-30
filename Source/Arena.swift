827f60d8447e2d41639e334710547ab5196d055e
Victor <vasilica.victor@gmail.com>
2015-04-30 23:19:39 +0300

    created files for the most important classes to be used

---
+++ Source/Arena.swift
@@ -0,0 +1,16 @@
+//
+//  Arena.swift
+//  MetalMindKit
+//
+//  Created by Victor Vasilica on 4/30/15.
+//  Copyright (c) 2015 simple. All rights reserved.
+//
+
+import Foundation
+
+public class MetalMindArena {
+    
+    public init() {
+        // init arena
+    }
+}
---
+++ Source/Skill.swift
@@ -0,0 +1,16 @@
+//
+//  Skill.swift
+//  MetalMindKit
+//
+//  Created by Victor Vasilica on 4/30/15.
+//  Copyright (c) 2015 simple. All rights reserved.
+//
+
+import Foundation
+
+public class MetalMindSkill {
+    
+    public init() {
+        // init skill
+    }
+}
